e79bf6458041f3a3c2617fd4db3334206397608f
wasilislam <wasilislam456@gmail.com>
2021-12-18 13:10:01 +0500

    second commit

--- utils/googleAuth.js
+++ utils/googleAuth.js
@@ -1,17 +1,16 @@
 const {OAuth2Client} = require("google-auth-library");
 const client = new OAuth2Client(process.env.CLIENT_ID);
-const getPayload=async (token)=>{
-    try {
-        const ticket = await client.verifyIdToken({
-          idToken: token,
-          audience: process.env.CLIENT_ID,
-        });
-        console.log(token,ticket,process.env.CLIENT_ID);
-        return ticket.getPayload();
-      } catch (err) {
-          console.error(err);
-        throw "$400 Error occured while logging in!"
-      }
-    
-}
+const getPayload = async (token) => {
+  try {
+    console.log(token);
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.CLIENT_ID,
+    });
+    return ticket.getPayload();
+  } catch (err) {
+    console.error(err);
+    throw "$400 Error occured while logging in!";
+  }
+};
 module.exports = {getPayload};
